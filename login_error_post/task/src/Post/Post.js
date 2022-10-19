437c73a24ec4a2109939e504107128ce5c114294
fatemeh ashrafi <fatemehashrafi305@gmail.com>
2022-10-19 00:33:53 -0700

    first commit

--- login_error_post/task/src/Post/Post.js
+++ login_error_post/task/src/Post/Post.js
@@ -18,40 +18,69 @@ function Post() {
     <div className='App'>
         <div className='addUser'>
 
-            <input type="text" placeholder='title...'
-            onChange={(e)=>{setTitle(e.target.value)}} />
-
-            <input type="text" placeholder='discription...' 
-            onChange={(e)=>{setDescription(e.target.value)}} />
-
-            <button onClick={()=>
-                {dispatch(addPost({id:postList[postList.length - 1].id +1 ,
-                title ,
-                description
-                }))}}>
-                Add User
-            </button>
+        <div className="mb-3 w-25 ">
+            <label for="exampleFormControlInput1" className="form-label">Title</label>
+            <input 
+            type="text" 
+            className="form-control " 
+            id="exampleFormControlInput1" 
+            placeholder="title..."
+            onChange={(e)=>{setTitle(e.target.value)}}/>
         </div>
-        <div className='displayUsers'>
+        <div className="mb-3 w-25 ">
+            <label for="exampleFormControlTextarea1" className="form-label">
+                Discription</label>
+            <textarea 
+            className="form-control" 
+            id="exampleFormControlTextarea1" 
+            placeholder='Discription post' 
+            rows="3"
+            type="text" 
+            onChange={(e)=>{setDescription(e.target.value)}}>
+            </textarea>
+        </div>
+        <div className="col-12 p-0">
+             <button 
+             type="submit" 
+             className="btn btn-primary "
+            onClick={()=>
+            {dispatch(addPost({id:postList[postList.length - 1].id +1 ,
+            title ,
+            description}))}}>
+            Add User</button>
+        </div>
+        </div>
+        
+        <div className='displayUsers '>
             {postList.map((user)=>{
                 return(
                     <div>
+                      
                         Title:<h3>{user.title}</h3>
                         Discription: <h3>{user.discription}</h3>
-                        
-                        <input type="text" placeholder=' new discription...' 
-                        onChange={(e)=>{setDiscriptionUpdate(e.target.value)}} 
-                        />
 
-                        <button onClick={()=>{dispatch(deletePost({id:user.id}))}}>
+                        <div class="input-group input-group-sm mb-3 w-25">
+                        <span 
+                        class="input-group-text" 
+                        id="inputGroup-sizing-sm">new discription</span>
+                        <input 
+                        type="text" 
+                        class="form-control" 
+                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                        onChange={(e)=>{setDiscriptionUpdate(e.target.value)}}/>
+                        </div>
+
+                        
+                        <button type="button" className="btn btn-danger btn-block m-2 p-1 "
+                         onClick={()=>{dispatch(deletePost({id:user.id}))}}>
                             delete
                         </button>
                         
-                        <button onClick={()=>
-                            {dispatch(updateDiscriptionPost({id:user.id , discription:discriptionUpdate }))}}>
+                        <button type="button" className="btn btn-warning btn-block m-2 p-1"
+                         onClick={()=> {dispatch(updateDiscriptionPost({id:user.id , discription:discriptionUpdate }))}}>
                             update
                         </button>
-
+                       
                     </div>
                 )
             })}
